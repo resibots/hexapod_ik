ff40cc5119f583f36e92d8a324667c13690621f2
Dorian Goepp <dorian.goepp@inria.fr>
2017-08-11 14:53:33 +0200

    cleanup legacy experimental code

--- src/multipod_ik_test.cpp
+++ src/multipod_ik_test.cpp
@@ -54,21 +54,6 @@ OF THE POSSIBILITY OF SUCH DAMAGE.
 // for stringstream
 #include <sstream>
 
-double fRand(double min, double max)
-{
-    double f = (double)rand() / RAND_MAX;
-    return min + f * (max - min);
-}
-
-// std::ostream& operator<<(std::ostream& in, const KDL::Vector& vector)
-// {
-//     in << std::setw(10) << vector.x() << "\t";
-//     in << std::setw(10) << vector.y() << "\t";
-//     in << std::setw(10) << vector.z();
-//
-//     return in;
-// }
-
 std::ostream& operator<<(std::ostream& in, const KDL::JntArray& jnt)
 {
     in << "[";
@@ -86,178 +71,6 @@ std::ostream& operator<<(std::ostream& in, const std::vector<KDL::Vector>& vecto
         in << vector << std::endl;
     }
     return in;
-}
-
-KDL::Frame joint_to_cartesian(std::shared_ptr<TRAC_IK::TRAC_IK> tracik_solver, KDL::JntArray joint_angles)
-{
-
-    // Get the KDL chain from URDF
-    // FIXME: we should not need to construct a TRAC_IK object for this
-    KDL::Chain chain;
-    bool valid = tracik_solver->getKDLChain(chain);
-
-    // Set up KDL forward kinematics to create goal end effector pose
-    KDL::ChainFkSolverPos_recursive fk_solver(chain); // Forward kin. solver
-
-    KDL::Frame frame;
-
-    int status = fk_solver.JntToCart(joint_angles, frame);
-    if (status < 0) {
-        ROS_ERROR_STREAM("Something went bad with the forward kinematics.");
-    }
-    return frame;
-}
-
-/** Get frame the end of a chain when all joints are at zero angle.
-
-    @param tracik_solver shared pointer to inverse kinematics solver
-
-    @return frame at the end of the chain
-**/
-KDL::Frame neutral_pose(std::shared_ptr<TRAC_IK::TRAC_IK> tracik_solver)
-{
-    // Get the KDL chain from URDF
-    // FIXME: we should not need to construct a TRAC_IK object for this
-    KDL::Chain chain;
-    bool valid = tracik_solver->getKDLChain(chain);
-    //
-    // // Set up KDL forward kinematics to create goal end effector pose
-    // KDL::ChainFkSolverPos_recursive fk_solver(chain); // Forward kin. solver
-    //
-    // Set joint angles to zero
-    KDL::JntArray joint_angles(chain.getNrOfJoints()); // set to zero at construction
-    //
-    // KDL::Frame frame;
-    //
-    // int status = fk_solver.JntToCart(joint_angles, frame);
-    // if (status < 0) {
-    //     ROS_ERROR_STREAM("Something went bad with the forward kinematics.");
-    // }
-    // return frame;
-    return joint_to_cartesian(tracik_solver, joint_angles);
-}
-
-bool test_fk(std::shared_ptr<TRAC_IK::TRAC_IK> tracik_solver)
-{
-    KDL::JntArray joint_angles(3);
-    joint_angles(0) = 0;
-    joint_angles(1) = 0;
-    joint_angles(2) = 0;
-
-    KDL::Frame frame = joint_to_cartesian(tracik_solver, joint_angles);
-
-    KDL::Vector point = frame.p;
-    KDL::Vector end_effector_pose(-0.173711, 0.233132, -0.115);
-    return (frame.p == end_effector_pose);
-}
-
-void test_ik(double num_samples, std::string chain_start, std::string chain_end, double timeout, std::string xml_string)
-{
-
-    double eps = 1e-5;
-
-    // This constructor parses the URDF loaded in rosparm urdf_param into the
-    // needed KDL structures.  We then pull these out to compare against the KDL
-    // IK solver.
-    TRAC_IK::TRAC_IK tracik_solver(xml_string, chain_start, chain_end, timeout, eps);
-
-    KDL::Chain chain;
-    KDL::JntArray ll, ul; //lower joint limits, upper joint limits
-
-    bool valid = tracik_solver.getKDLChain(chain);
-
-    if (!valid) {
-        ROS_ERROR("There was no valid KDL chain found");
-        return;
-    }
-
-    valid = tracik_solver.getKDLLimits(ll, ul);
-
-    if (!valid) {
-        ROS_ERROR("There were no valid KDL joint limits found");
-        return;
-    }
-
-    assert(chain.getNrOfJoints() == ll.data.size());
-    assert(chain.getNrOfJoints() == ul.data.size());
-
-    ROS_INFO("Using %d joints", chain.getNrOfJoints());
-
-    // Set up KDL forward kinematics to create goal end effector pose
-    KDL::ChainFkSolverPos_recursive fk_solver(chain); // Forward kin. solver
-
-    // Create Nominal chain configuration midway between all joint limits
-    KDL::JntArray nominal(chain.getNrOfJoints());
-
-    for (uint j = 0; j < nominal.data.size(); j++) {
-        nominal(j) = (ll(j) + ul(j)) / 2.0;
-    }
-
-    // Create desired number of valid, random joint configurations
-    std::vector<KDL::JntArray> JointList;
-    KDL::JntArray q(chain.getNrOfJoints());
-
-    for (uint i = 0; i < num_samples; i++) {
-        for (uint j = 0; j < ll.data.size(); j++) {
-            q(j) = fRand(ll(j), ul(j));
-        }
-        JointList.push_back(q);
-    }
-
-    boost::posix_time::ptime start_time;
-    boost::posix_time::time_duration diff;
-
-    KDL::JntArray result;
-    KDL::Frame end_effector_pose;
-    int rc;
-
-    double total_time = 0;
-    uint success = 0;
-    //
-    // ROS_INFO_STREAM("*** Testing KDL with " << num_samples << " random samples");
-    //
-    // for (uint i = 0; i < num_samples; i++) {
-    //     fk_solver.JntToCart(JointList[i], end_effector_pose);
-    //     double elapsed = 0;
-    //     result = nominal; // start with nominal
-    //     start_time = boost::posix_time::microsec_clock::local_time();
-    //     do {
-    //         q = result; // when iterating start with last solution
-    //         rc = kdl_solver.CartToJnt(q, end_effector_pose, result);
-    //         diff = boost::posix_time::microsec_clock::local_time() - start_time;
-    //         elapsed = diff.total_nanoseconds() / 1e9;
-    //     } while (rc < 0 && elapsed < timeout);
-    //     total_time += elapsed;
-    //     if (rc >= 0)
-    //         success++;
-    //
-    //     if (int((double)i / num_samples * 100) % 10 == 0)
-    //         ROS_INFO_STREAM_THROTTLE(1, int((i) / num_samples * 100) << "\% done");
-    // }
-    //
-    // ROS_INFO_STREAM("KDL found " << success << " solutions (" << 100.0 * success / num_samples << "\%) with an average of " << total_time / num_samples << " secs per sample");
-    //
-    // total_time = 0;
-    // success = 0;
-
-    ROS_INFO_STREAM("*** Testing TRAC-IK with " << num_samples << " random samples");
-
-    for (uint i = 0; i < num_samples; i++) {
-        fk_solver.JntToCart(JointList[i], end_effector_pose);
-        double elapsed = 0;
-        start_time = boost::posix_time::microsec_clock::local_time();
-        rc = tracik_solver.CartToJnt(nominal, end_effector_pose, result);
-        diff = boost::posix_time::microsec_clock::local_time() - start_time;
-        elapsed = diff.total_nanoseconds() / 1e9;
-        total_time += elapsed;
-        if (rc >= 0)
-            success++;
-
-        if (int((double)i / num_samples * 100) % 10 == 0)
-            ROS_INFO_STREAM_THROTTLE(1, int((i) / num_samples * 100) << "\% done");
-    }
-
-    ROS_INFO_STREAM("TRAC-IK found " << success << " solutions (" << 100.0 * success / num_samples << "\%) with an average of " << total_time / num_samples << " secs per sample");
 }
 
 std::vector<KDL::Frame> generate_cartesian_traj(const double t)
@@ -301,43 +114,6 @@ std::vector<KDL::Frame> generate_cartesian_traj(const double t)
     }
 
     return feet;
-}
-
-std::vector<KDL::JntArray> generate_joint_traj(
-    std::shared_ptr<TRAC_IK::TRAC_IK> tracik_solver,
-    const std::vector<KDL::Frame>& frames,
-    const std::vector<KDL::JntArray>& nominals)
-{
-
-    // Set rotational bounds to max, so that the kinematics ignores the rotation
-    // part of the target pose
-    KDL::Twist bounds = KDL::Twist::Zero();
-    bounds.rot.x(std::numeric_limits<float>::max());
-    bounds.rot.y(std::numeric_limits<float>::max());
-    bounds.rot.z(std::numeric_limits<float>::max());
-
-    assert(frames.size() == nominals.size());
-
-    std::vector<KDL::JntArray> results;
-    KDL::JntArray result;
-
-    for (size_t i = 0; i < frames.size(); ++i) {
-        std::cout << "Nominal joint angles: " << nominals.at(i) << std::endl;
-        std::cout << "Target position: " << frames.at(i).p << std::endl;
-        if (!tracik_solver->CartToJnt(nominals.at(i), frames.at(i), result, bounds))
-            std::cerr << "Failed to solve inverse kinematics problem" << std::endl;
-        else
-            results.push_back(result);
-
-        std::cout << "Joint coordinates: " << result << std::endl;
-        std::cout << "Related position:  "
-                  << joint_to_cartesian(tracik_solver, result).p
-                  << std::endl
-                  << std::endl;
-        // break;
-    }
-
-    return results;
 }
 
 int main(int argc, char** argv)
@@ -474,17 +250,10 @@ int main(int argc, char** argv)
             state.header.stamp = ros::Time::now();
             joint_states.publish(state);
         }
-        // else
-        //     ros::Duration(3).sleep();
 
         ros::spinOnce();
         r.sleep();
     }
-
-    // std::vector<KDL::JntArray> angles = generate_joint_traj(
-    //     tracik_solvers[0],
-    //     frames,
-    //     nominals);
 
     return 0;
 }
